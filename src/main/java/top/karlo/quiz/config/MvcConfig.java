6d94053d34aa73f0d3402fea92039a33a57007bc
赵治博 <wenshan747@126.com>
2019-01-03 17:38:37 +0800

    页面初步布局

--- src/main/java/top/karlo/quiz/config/MvcConfig.java
+++ src/main/java/top/karlo/quiz/config/MvcConfig.java
@@ -19,7 +19,7 @@ public class MvcConfig  implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/layui").setViewName("layui");
+        registry.addViewController("/quiz").setViewName("quiz");
     }
 
 }
